9edc420a2a4f5b3315f730be940320f3d3ac2180
onelky <onelky.hernandez29@gmail.com>
2024-02-05 12:33:23 -0400

    added Configuration to allow request to auth endpoints only

--- src/main/java/onelky/chatapp/channel/ChannelController.java
+++ src/main/java/onelky/chatapp/channel/ChannelController.java
@@ -1,11 +1,19 @@
 package onelky.chatapp.channel;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
-@RequestMapping("/channel")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class ChannelController {
+    @GetMapping("/channels")
+    public List<String> getChannels(){
+        return  Arrays.asList("Channel 1", "Channel 2", "Channel 3");
+    }
 }
---
+++ src/main/java/onelky/chatapp/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package onelky.chatapp.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authRequest ->
+                  authRequest
+                   .requestMatchers("/auth/**").permitAll()
+                   .anyRequest().authenticated()
+                ).build();
+    }
+}
