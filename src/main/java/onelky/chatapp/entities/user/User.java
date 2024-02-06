600dd4dbaeda833778500ae7a7ba697d97c8dc2e
onelky <onelky.hernandez29@gmail.com>
2024-02-05 20:46:11 -0400

    created User entity and repository

---
+++ src/main/java/onelky/chatapp/entities/user/User.java
@@ -0,0 +1,60 @@
+package onelky.chatapp.entities.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user",uniqueConstraints = {@UniqueConstraint(columnNames = {"username", "email"})} )
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue
+    Integer id;
+    @Column(nullable = false)
+    String username;
+
+    @Column(nullable = false)
+    String password;
+    @Column(nullable = false)
+    String email;
+
+    @Enumerated(EnumType.STRING)
+    Provider provider;
+
+    String photoUrl;
+    String halt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
