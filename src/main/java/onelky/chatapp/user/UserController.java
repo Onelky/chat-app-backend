4104f28a850a7478fd0e6a9abed7ff841c66ac78
onelky <onelky.hernandez29@gmail.com>
2024-02-09 11:57:03 -0400

    included ExceptionHandler inside UserController

--- src/main/java/onelky/chatapp/user/UserController.java
+++ src/main/java/onelky/chatapp/user/UserController.java
@@ -1,11 +1,25 @@
 package onelky.chatapp.user;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import onelky.chatapp.common.entities.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+    @PutMapping("/{id}")
+    public ResponseEntity<UpdateUserRequest> update(@Valid @RequestBody UpdateUserRequest request, @PathVariable("id") int id){
+        return ResponseEntity.ok(userService.update(id, request));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidRequestException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(ex.getMessage()) );
+    }
+
 }
