a94b8a4c933c2918dc0f6415fb9816598fafe0b5
onelky <onelky.hernandez29@gmail.com>
2024-02-15 18:22:05 -0400

    created user repository tests

---
+++ src/test/java/onelky/chatapp/user/UserRepositoryTests.java
@@ -0,0 +1,52 @@
+package onelky.chatapp.user;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2, replace = AutoConfigureTestDatabase.Replace.ANY)
+class UserRepositoryTests {
+
+	@Autowired
+	private UserRepository userRepository;
+	private User user;
+
+	@BeforeEach
+	public void setup() {
+		user = User.builder()
+				.email("test@gmail.com")
+				.username("test")
+				.provider(Provider.LOCAL)
+				.password("testpassword")
+				.build();
+
+	}
+
+	@Test
+	public void userRepository_save_returnsSavedUser(){
+
+		User savedUser =  userRepository.save(user);
+		assertThat(savedUser).isNotNull();
+		assertThat(savedUser.getId()).isGreaterThan(0);
+	}
+
+	@Test
+	public void userRepository_findByUsername_returnsOneUser(){
+
+		User savedUser =  userRepository.save(user);
+		User user = userRepository.findByUsername(savedUser.getUsername());
+        assertThat(user).isNotNull();
+        assertThat(user.getUsername()).isEqualTo("test");
+	}
+
+}
